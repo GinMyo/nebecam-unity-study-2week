670252fc6bfcce25eddf19e9667fb6b531e3b532
GinMyo <rlsayay101@gmail.com>
2025-07-04 19:12:54 +0900

    쉴드 스프라이트 마우스 포지션과 같게 움직이게 하기

---
+++ Assets/001.Scripts/Shield.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+
+    private void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
+    }
+}
